bbea76fbe8251e1ed973e83bcc627324941c0b38
Tanish Pandey <tanishpandey14@gmail.com>
2024-05-17 20:45:26 +0530

    class base component practice

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ClassProps from './ComPonents/ClassProps'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ClassProps name= "tanish"/>
+      </div>
+    )
+  }
+}
