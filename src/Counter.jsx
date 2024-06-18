773efeb60a25a3fdc6bf23cecfbd648e3133a6e9
Tanish Pandey <tanishpandey14@gmail.com>
2024-06-18 16:58:36 +0530

    js

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Counter extends Component {
     constructor(props){
         super(props)
-        this.state = {count : 0}
+        this.state = {count : 10}
     }
 
     increment = () => {
