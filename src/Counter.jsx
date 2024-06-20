3b0bc3bad3fb98c429cc74b89049d417931e9fda
Tanish Pandey <tanishpandey14@gmail.com>
2024-06-20 18:32:12 +0530

    js

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,16 +3,21 @@ import React, { Component } from 'react'
 export default class Counter extends Component {
     constructor(props){
         super(props)
-        this.state = {count : 10}
+        this.state = {count : 0}
     }
 
     increment = () => {
-        this.setState({count : this.state.count+1})
-    }
-    decrement = () => {
-        this.setState({count : this.state.count-1})
-    }
-
+      this.setState({count : this.state.count+1})
+  }
+  decrement = () => {
+      this.setState({count : this.state.count-1})
+  }
+  increment = () => {
+    this.setState({count : this.state.count+1})
+}
+decrement = () => {
+    this.setState({count : this.state.count-1})
+}
 
   render() {
     return (
