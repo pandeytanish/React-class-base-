dde844a35c1d090f12be02f1dbf2c60950ac84f1
Tanish Pandey <tanishpandey14@gmail.com>
2024-06-27 22:10:18 +0530

    js

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -13,6 +13,12 @@ function Todo() {
                 </div>
 
                 <div>
+                    <h1>TODO LIST</h1>
+                </div>
+                <div>
+                    <h1>TODO LIST</h1>
+                </div>
+                <div>
                     <input value={inputData} onChange={(e) => setInputData(e.target.value)} type='text' placeholder='add todo '> <button>Add item</button></input>
                 </div>
                 <div>
