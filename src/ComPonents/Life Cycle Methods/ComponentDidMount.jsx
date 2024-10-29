d52449fb87cc287a87ff27a4e51681f1de2a6499
Tanish Pandey <tanishpandey14@gmail.com>
2024-10-29 21:11:33 +0530

    enzyme

--- src/ComPonents/Life Cycle Methods/ComponentDidMount.jsx
+++ src/ComPonents/Life Cycle Methods/ComponentDidMount.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 
 export default class ComponentDidMount extends Component {
-    constructor() {
-        super();
-        this.state = {
-            name : 'Tanish'
-        }
+  constructor() {
+    super();
+    this.state = {
+      name: 'Tanish'
     }
-    componentDidMount() {
-   console.warn("componentDidMount")
-    }
+  }
+  componentDidMount() {
+    console.warn("componentDidMount")
+  }
   render() {
     console.warn("render")
     return (
       <div>
         <h1>My Name is {this.state.name}</h1>
-        <button onClick={()=>{this.setState({name: 'Trishal'})}}>Update</button>
+        <h1>My Name is {this.state.name}</h1>
+
+        <button onClick={() => { this.setState({ name: 'Trishal' }) }}>Update</button>
       </div>
     )
   }
